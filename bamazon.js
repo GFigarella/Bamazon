d86bdf9422429244d33512d0c4427ca976dc8342
Gabriel Figarella <GFigarella@Gabriels-MacBook-Pro.local>
2018-01-16 01:44:50 -0500

    added most of the code for the CONSUMER part, having issues with updating the database

--- bamazon.js
+++ bamazon.js
@@ -1,7 +1,9 @@
-import { userInfo } from "os";
+// import { userInfo } from "os";
 
+// require needed npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require("console.table");
 
 //connect to our bamazonDB
 var connection = mysql.createConnection({
@@ -14,7 +16,7 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    user();
+    firstPrompt();
 })
 
 // first function that asks the user how he wants to log in: as a user, manager or supervisor
@@ -23,24 +25,74 @@ function firstPrompt() {
         name: "first",
         type: "rawlist",
         message: "What would you like to log in as?",
-        choices: ["USER", "MANAGER", "SUPERVISOR"]
+        choices: ["CONSUMER", "MANAGER", "SUPERVISOR"]
     }).then(answers => {
-        if (answers.first === "USER") {
-            user();
+        if (answers.first === "CONSUMER") {
+            //Show the table of items so that the user knows what's available for sale
+            connection.query("SELECT * FROM items_for_sale", function (err, res) {
+                if (err) throw err;
+                // loop that prints the items for sale
+                var myArr = [];
+                for (i = 0; i<res.length; i++){
+                    var values = [res[i].id, res[i].item, res[i].department, res[i].price, res[i].quantity];
+                    myArr.push(values);
+                }
+                console.table(['id', 'item', 'department', 'price', 'quantity'], myArr);
+                consumer();
+            });
         }
+
         else if (answers.first === "MANAGER") {
-            multi();
+            console.log("Coming soon");
+            connection.end();
         }
         else if (answers.first === "SUPERVISOR") {
-            specific();
+            console.log("Coming soon");
+            connection.end();
         }
     })
 }
 
 
-function user(){
-    inquirer.prompt({
-        name: "item",
-        message: "What item would you like to purchase? (Select by ID number)"
-    })
+function consumer(){
+    //ask the user what item he/she wants to buy and how many
+    inquirer.prompt([
+        {
+          name: "id",
+          message: "What item would you like to purchase? (Select by ID number)"
+        }, {
+          name: "number",
+          message: "How many do you want to purchase?"
+    }]).then(function(answers){
+        // check if there's enough items to buy left
+        connection.query("SELECT * FROM items_for_sale WHERE id=?", answers.id, function(err, res){
+            //if there's not enough items in stock, let the consumer know
+            if (answers.number > res[0].quantity){
+                console.log("\nThat's more than what we currently have in stock! Please buy less\n");
+                consumer();
+            }
+            //if there's enough items in stock, confirm purchase
+            else{
+                console.log("\n" + answers.number + " " + res[0].item +"(s/es/ies) purchased!\n");
+                var newAmount = res[0].item - answers.number;
+                //update database after purchase
+                connection.query(
+                    "UPDATE auctions SET ? WHERE ?",
+                    [
+                      {
+                        quantity: answers.bid
+                      },
+                      {
+                        id: answers.id
+                      }
+                    ],
+                    function(error) {
+                      if (error) throw err;
+                      console.log("Bid placed successfully!");
+                      start();
+                    }
+                );
+            }
+        });
+    });
 }
